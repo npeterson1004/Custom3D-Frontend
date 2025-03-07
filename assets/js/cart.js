ead5dd868c44a906ffaa481c8006583792f377cb
npeterson1004 <npeterson1004@gmail.com>
2025-03-07 14:35:51 -0600

    adding payment features

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -87,7 +87,10 @@ function loadCart() {
                     <td><img src="${item.color.image}"  class="cart-item-img"></td>
                     <td>$${item.price.toFixed(2)}</td>
                     <td>${item.color.name}</td>
-                    <td>${item.quantity}</td>
+                    <td>
+                        <input type="number" class="form-control cart-quantity" min="1" value="${item.quantity}" 
+                            onchange="updateCartItem(${index}, this.value)">
+                    </td>
                     <td>$${itemTotal.toFixed(2)}</td>
                     <td><button class="btn btn-danger btn-sm" onclick="removeFromCart(${index})">Remove</button></td>
                 </tr>
